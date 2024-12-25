e906567be7c3851898e2ffcdf24955aed6e5b4a4
Ruturaj Mestry <mestryruturaj@gmail.com>
2024-12-25 11:56:38 +0530

    Dev: Model

--- src/main/java/io/rescrypt/batch/job/practice/batch_job_practice/model/Product.java
+++ src/main/java/io/rescrypt/batch/job/practice/batch_job_practice/model/Product.java
@@ -1,5 +1,9 @@
 package io.rescrypt.batch.job.practice.batch_job_practice.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Product {
+    @Id
     private int productId;
     private String title;
     private String description;
     private Double price;
     private Double discount;
+    @Transient
     private Double discountedPrice;
 }
