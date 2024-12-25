5cc112d2f8d9d59669d44a8c193b5750ec8ffa4a
Ruturaj Mestry <mestryruturaj@gmail.com>
2024-12-25 11:57:11 +0530

    Dev: Batch Job creation

--- src/main/java/io/rescrypt/batch/job/practice/batch_job_practice/BatchJobPracticeApplication.java
+++ src/main/java/io/rescrypt/batch/job/practice/batch_job_practice/BatchJobPracticeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BatchJobPracticeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BatchJobPracticeApplication.class, args);
 	}
-
 }
--- src/main/java/io/rescrypt/batch/job/practice/batch_job_practice/config/batch/JobExecutionListenerImpl.java
+++ src/main/java/io/rescrypt/batch/job/practice/batch_job_practice/config/batch/JobExecutionListenerImpl.java
@@ -1,6 +1,8 @@
-package io.rescrypt.batch.job.practice.batch_job_practice.config;
+package io.rescrypt.batch.job.practice.batch_job_practice.config.batch;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
@@ -9,15 +11,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class JobExecutionListenerImpl implements JobExecutionListener {
+
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        log.info(String.format("Batch job %s has started.", jobExecution.getJobInstance().getJobName()))
+        log.info(String.format("Batch job %s has started.", jobExecution.getJobInstance().getJobName()));
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus().equals(BatchStatus.COMPLETED)) {
-            log.info(String.format("Batch job %s has completed.", jobExecution.getJobInstance().getJobName()))
+            log.info(String.format("Batch job %s has completed.", jobExecution.getJobInstance().getJobName()));
         }
     }
 }
