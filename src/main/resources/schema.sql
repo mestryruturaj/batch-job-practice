78a428753604f48f47a641186894d2b84d74b442
Ruturaj Mestry <mestryruturaj@gmail.com>
2024-12-25 11:55:35 +0530

    Dev: Config and resources

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,1 @@
-create table if not exists product (id int primary key, title varchar(128), description varchar(512), prices decimal(7,2), discount decimal (3,2), discounted_price decimal(7,2));
+create table if not exists product (product_id int primary key auto_increment, title varchar(128), description varchar(512), price decimal(7,2), discount decimal (7,2), discounted_price decimal(7,2));
